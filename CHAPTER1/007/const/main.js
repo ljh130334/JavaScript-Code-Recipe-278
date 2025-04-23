285266f8fa6a1144d2b6f427a92c88243974a939
fr0gydev <ljh130334@naver.com>
2025-04-23 15:18:50 +0900

    feat: 007. 상수 사용하기

---
+++ CHAPTER1/007/const/main.js
@@ -0,0 +1,18 @@
+const myPet = '사자';
+alert(myPet);
+
+const myString = '사자';
+const myFunction = () => console.log('함수 확인');
+
+const myName = '지현';
+// myName = '가현';
+
+const number1 = 10;
+const number2 = 20;
+const sum = number1 + number2;
+console.log(sum);
+
+const familyName = '이';
+const firstName = '지현';
+const fullName = familyName + firstName;
+console.log(fullName)
