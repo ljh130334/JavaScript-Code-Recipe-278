fcd598c693fa518011f76d770a4addd59a1d82ab
fr0gydev <ljh130334@naver.com>
2025-04-23 15:13:04 +0900

    feat: 006. 변수 선언하기

---
+++ CHAPTER1/006/let/main.js
@@ -0,0 +1,6 @@
+let myName = "제이펍";
+alert(myName);
+
+let myString = '제이펍';
+let currentDate = new Date();
+let myFunction = () => console.log('함수 확인');
---
+++ CHAPTER1/006/let_2/main.js
@@ -0,0 +1,13 @@
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
+
+let firstName = "이";
+let familyName = "지현";
+let fullName = firstName + familyName;
+console.log(fullName);
+
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
