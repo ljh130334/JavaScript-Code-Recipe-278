98e6370f296741963cb0edb93584e7c31ed39ecf
fr0gydev <ljh130334@naver.com>
2025-04-24 20:10:09 +0900

    feat: 010. 복합형 대입 연산자 사용하기

---
+++ CHAPTER1/010/main.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+a += b;
+console.log(a); // 30
+
+let c = '자바';
+let d = '스크립트';
+c += d;
+console.log(c); // 자바스크립트
+
+let e = 5;
+let f = 2;
+e *= f;
+console.log(e); // 10
