efe38e9b96c97987fd5acacccb410458f37f1f04
fr0gydev <ljh130334@naver.com>
2025-04-25 23:37:37 +0900

    feat: 012. 화살표 함수 사용하기

---
+++ CHAPTER1/012/main.js
@@ -0,0 +1,14 @@
+const calcSum = (a, b, c) => {
+    const result = a + b + c;
+    return result;
+}
+
+const myFunction1 = (a) => {
+    return a + 2;
+}
+
+const myFunction2 = a => {
+    return a + 2;
+}
+
+const myFunction3 = (a) => a + 2;
